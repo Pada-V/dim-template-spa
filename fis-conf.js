f6565433f0432ec1f4577c0c23f43c205f7c1b59
jerrold01.li <jerrold01.li@vipshop.com>
2015-06-13 16:04:31 +0800

    add support of less

--- fis-conf.js
+++ fis-conf.js
@@ -1,23 +1,27 @@
 var meta = require('./package.json');
+
+fis.config.set("modules.parser.less", "less");
+fis.config.set("roadmap.ext.less", "css");
+
 fis.config.set('name', meta.name);
 fis.config.set('version', meta.version);
 fis.config.set('project.exclude', 'node_modules/**');
+
+fis.config.get("roadmap.path").unshift({
+    "reg":/^\/components\/(.*)\.(less|css)$/i,
+    "id":"${name}/${version}/$1.css",
+    "isMod":true,
+    "useSprite":true,
+    "useHash":false,
+    "url":"${urlPrefix}/c/${name}/${version}/$1.$2",
+    "release":"/public/c/${name}/${version}/$1.$2"
+});
+
 fis.config.set('framework', {
     cache: true,
     urlPattern: '/c/%s',
     comboPattern: '/co??%s',
     alias: {
-    	//'$': 			'components/spa/lib/zepto.js',
-    	//'fastclick': 	'components/spa/lib/fastclick.js',
-    	//'template': 	'components/spa/lib/template.js',
-    	//'basepage': 	'components/spa/core/basepage.js',
-        //'baseapi':      'components/spa/core/baseapi.js',
-    	//'spaconfig': 	'components/spa/core/spaconfig.js',
-    	//'runtime': 		'components/spa/core/runtime.js',
-    	//'tploader': 	'components/spa/core/tploader.js',
-    	//'dispatcher': 	'components/spa/core/dispatcher.js',
-    	//'clearout': 	'components/spa/widget/clearout.js',
-    	//'scroller': 	'components/spa/widget/scroller.js',
-        //'listview':     'components/spa/widget/listview.js'
+        
     }
 });
